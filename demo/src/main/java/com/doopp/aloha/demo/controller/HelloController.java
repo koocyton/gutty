db3b4105b78279cfd21688ca809a6d24a4a0aab9
liuyi <liuyi@avglife.cn>
2021-01-13 18:07:04 +0800

    完成注入的解析

--- demo/src/main/java/com/doopp/aloha/demo/controller/HelloController.java
+++ demo/src/main/java/com/doopp/aloha/demo/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.doopp.aloha.demo.controller;
 
 import com.doopp.aloha.framework.annotation.Controller;
+import com.google.inject.Inject;
+import io.netty.channel.EventLoopGroup;
 
+import javax.inject.Named;
 import javax.ws.rs.Path;
 
 @Path("")
 @Controller
 public class HelloController {
+
+    @Inject
+    @Named("executeGroup")
+    private EventLoopGroup executeGroup;
 
     @Path("")
     public String a() {
