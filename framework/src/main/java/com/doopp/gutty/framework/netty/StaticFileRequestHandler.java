ac4daadb053a5a63f32ea243a74524e345896e36
koocyton <koocyton@gmail.com>
2021-01-22 17:37:10 +0800

    keepAlive 优化

--- framework/src/main/java/com/doopp/gutty/framework/netty/StaticFileRequestHandler.java
+++ framework/src/main/java/com/doopp/gutty/framework/netty/StaticFileRequestHandler.java
@@ -3,26 +3,11 @@ package com.doopp.gutty.framework.netty;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.activation.MimetypesFileTypeMap;
 import java.io.*;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-import static io.netty.handler.codec.http.HttpResponseStatus.*;
-import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class StaticFileRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
-
-    private final static Logger logger = LoggerFactory.getLogger(StaticFileRequestHandler.class);
-
-    public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
-    public static final String HTTP_DATE_GMT_TIMEZONE = "GMT";
-    public static final int HTTP_CACHE_SECONDS = 3600;
 
     private static final MimetypesFileTypeMap mimetypesFileTypeMap = new MimetypesFileTypeMap();
 
@@ -55,100 +40,19 @@ public class StaticFileRequestHandler extends SimpleChannelInboundHandler<FullHt
         httpResponse.headers().set(HttpHeaderNames.CONTENT_LENGTH, httpResponse.content().readableBytes());
         // media type
         httpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE, mimetypesFileTypeMap.getContentType(requestUri));
-        //
-        if (HttpUtil.isKeepAlive(httpRequest)) {
+        // keepAlive
+        boolean keepAlive = HttpUtil.isKeepAlive(httpRequest);
+        if (!keepAlive) {
+            httpResponse.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+        }
+        else if (httpRequest.protocolVersion().equals(HttpVersion.HTTP_1_0)) {
             httpResponse.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
         }
-
+        // ctx write
         ctx.write(httpResponse);
         ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
-        if (!HttpUtil.isKeepAlive(httpRequest)) {
+        if (!keepAlive) {
             future.addListener(ChannelFutureListener.CLOSE);
         }
-    }
-
-    private File staticFileByUri(String requestUri) {
-        int indexOf = requestUri.indexOf("?");
-        if (indexOf!=-1)
-            requestUri = requestUri.substring(0, indexOf);
-        // get resource
-        URL resource = StaticFileRequestHandler.class.getResource(requestUri);
-        if (resource==null) {
-            return null;
-        }
-        // 将静态文件读取
-        File staticFile = new File(resource.getFile());
-        if (!staticFile.canRead() || staticFile.isDirectory()) {
-            return null;
-        }
-        return staticFile;
-    }
-
-    private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
-        FullHttpResponse response = new DefaultFullHttpResponse(
-                HTTP_1_1, status, Unpooled.copiedBuffer("Failure: " + status + "\r\n", CharsetUtil.UTF_8));
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
-
-        // Close the connection as soon as the error message is sent.
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
-
-    /**
-     * When file timestamp is the same as what the browser is sending up, send a "304 Not Modified"
-     *
-     * @param ctx Context
-     */
-    public static void sendNotModified(ChannelHandlerContext ctx) {
-        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, NOT_MODIFIED);
-        setDateHeader(response);
-
-        // Close the connection as soon as the error message is sent.
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
-
-    /**
-     * Sets the Date header for the HTTP response
-     *
-     * @param response HTTP response
-     */
-    private static void setDateHeader(FullHttpResponse response) {
-        SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
-        dateFormatter.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
-
-        Calendar time = new GregorianCalendar();
-        response.headers().set(HttpHeaderNames.DATE, dateFormatter.format(time.getTime()));
-    }
-
-    /**
-     * Sets the Date and Cache headers for the HTTP Response
-     *
-     * @param response    HTTP response
-     * @param fileToCache file to extract content type
-     */
-    private static void setDateAndCacheHeaders(HttpResponse response, File fileToCache) {
-        SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
-        dateFormatter.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
-
-        // Date header
-        Calendar time = new GregorianCalendar();
-        response.headers().set(HttpHeaderNames.DATE, dateFormatter.format(time.getTime()));
-
-        // Add cache headers
-        time.add(Calendar.SECOND, HTTP_CACHE_SECONDS);
-        response.headers().set(HttpHeaderNames.EXPIRES, dateFormatter.format(time.getTime()));
-        response.headers().set(HttpHeaderNames.CACHE_CONTROL, "private, max-age=" + HTTP_CACHE_SECONDS);
-        response.headers().set(
-                HttpHeaderNames.LAST_MODIFIED, dateFormatter.format(new Date(fileToCache.lastModified())));
-    }
-
-    /**
-     * Sets the content type header for the HTTP Response
-     *
-     * @param response HTTP response
-     * @param filePath file path
-     */
-    private static void setContentTypeHeader(HttpResponse response, String filePath, MimetypesFileTypeMap mimetypesFileTypeMap) {
-        // MimetypesFileTypeMap mimetypesFileTypeMap = new MimetypesFileTypeMap();
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, mimetypesFileTypeMap.getContentType(filePath));
     }
 }
