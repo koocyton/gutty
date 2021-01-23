042ff662600b5e7b7f60c4041e8cf881a9231c81
koocyton <koocyton@gmail.com>
2021-01-24 01:45:12 +0800

    websocket

--- framework/src/main/java/com/doopp/gutty/framework/netty/WebSocketConnectHandler.java
+++ framework/src/main/java/com/doopp/gutty/framework/netty/WebSocketConnectHandler.java
@@ -2,43 +2,44 @@ package com.doopp.gutty.framework.netty;
 
 import com.doopp.gutty.framework.Dispatcher;
 import com.google.inject.Injector;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WebSocketConnectHandler extends SimpleChannelInboundHandler<Object> {
+import java.util.List;
+
+public class WebSocketConnectHandler extends WebSocketServerProtocolHandler {
 
     private final static Logger logger = LoggerFactory.getLogger(WebSocketConnectHandler.class);
 
-    private final Injector injector;
+    private Injector injector;
 
     public WebSocketConnectHandler(Injector injector){
+        super("");
         this.injector = injector;
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
-        if (msg instanceof FullHttpRequest){
-            onConnect(ctx, (FullHttpRequest) msg);
+    protected void decode(ChannelHandlerContext ctx, WebSocketFrame frame, List<Object> out) throws Exception {
+        if (frame instanceof CloseWebSocketFrame) {
+            WebSocketServerHandshaker handshaker = getHandshaker(ctx.channel());
+            if (handshaker != null) {
+                frame.retain();
+                handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame);
+            } else {
+                logger.info("{}", ctx);
+                ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+            }
+            return;
         }
-        else if (msg instanceof TextWebSocketFrame){
-            onTextMessage(ctx, (TextWebSocketFrame) msg);
-        }
-        else if (msg instanceof BinaryWebSocketFrame){
-            onBinaryMessage(ctx, (BinaryWebSocketFrame) msg);
-        }
-        else if (msg instanceof PingWebSocketFrame){
-            onPing(ctx, (PingWebSocketFrame) msg);
-        }
-        else if (msg instanceof PongWebSocketFrame){
-            onPong(ctx, (PongWebSocketFrame) msg);
-        }
-        else if (msg instanceof CloseWebSocketFrame){
-            onClose(ctx, (CloseWebSocketFrame) msg);
-        }
+        super.decode(ctx, frame, out);
     }
 
     private void onConnect(ChannelHandlerContext ctx, FullHttpRequest httpRequest) {
@@ -69,5 +70,16 @@ public class WebSocketConnectHandler extends SimpleChannelInboundHandler<Object>
 
     private void onPong(ChannelHandlerContext ctx, PongWebSocketFrame socketFrame) {
 
+    }
+
+    private static final AttributeKey<WebSocketServerHandshaker> HANDSHAKER_ATTR_KEY =
+            AttributeKey.valueOf(WebSocketServerHandshaker.class, "HANDSHAKER");
+
+    static WebSocketServerHandshaker getHandshaker(Channel channel) {
+        return channel.attr(HANDSHAKER_ATTR_KEY).get();
+    }
+
+    static void setHandshaker(Channel channel, WebSocketServerHandshaker handshaker) {
+        channel.attr(HANDSHAKER_ATTR_KEY).set(handshaker);
     }
 }
