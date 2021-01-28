e00ff531ed3099d2e1c29c634b2a0b25df760473
koocyton <koocyton@gmail.com>
2021-01-28 20:05:46 +0800

    增加消息 jsonMessage 和 protobufMessage

---
+++ framework/src/main/java/com/doopp/gutty/framework/annotation/websocket/JsonMessage.java
@@ -0,0 +1,12 @@
+package com.doopp.gutty.framework.annotation.websocket;
+
+import javax.ws.rs.HttpMethod;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod(HttpMethod.GET)
+@Documented
+public @interface JsonMessage {
+}
+
